5bdf01cc2b873b0fe2932d19ba50e858af2b6cae
Boop-Py <60817228+Boop-Py@users.noreply.github.com>
2020-02-17 18:21:07 +0000

    update square loop
    Catches if a user input is not a number.

--- square_loop.py
+++ square_loop.py
@@ -1,8 +1,11 @@
-limit = input("What is your limit? ")
-
-for i in range(0, limit):
-    squared_i = i*i
-    print(i, squared_i)
-    if squared_i >= 200:
-        print("You've reached 200 or more. Ending loop.")
-        break
+try:
+    limit = int(input("What is your limit? "))   
+except ValueError:
+    print("Nope. Try again.")      
+else:
+    for i in range(0, limit):
+        squared_i = i*i
+        print(i, squared_i)
+        if squared_i >= 200:
+            print("You've reached 200 or more. Ending loop.")
+            break
